ceacf6fcc7d01cb9d61e0b9cd04459101b63369a
nam1509 <mylovenam126@gmail.com>
2022-04-24 22:54:35 +0700

    เพิ่มไอคอน

--- miniproject/frontend/pages/login.js
+++ miniproject/frontend/pages/login.js
@@ -31,6 +31,10 @@ export default function Login({ token }) {
     const loginForm = () => (
 
         <div className=" bg-amber-300 shadow rounded-lg lg:w-1/3  md:w-1/2 w-full p-4 mt-16 relative m-4   flex flex-col items-center justify-center">
+            <div>  <img class="sm:w-1/6 sm:h-1/6 items-center " src="https://cdn-icons.flaticon.com/png/512/2276/premium/2276931.png?token=exp=1650816311~hmac=6d900f76801498d89895213ff3854eb4" /> 
+                <h3 class="text-2xl font-bold text-center shadow">Login to your account</h3>
+                   
+                    </div> 
             <div>
                 Username:
             </div>
