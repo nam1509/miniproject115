42f2fc361aa3d27e793cb9d8472fe7b95af30ab9
nam1509 <mylovenam126@gmail.com>
2022-04-24 23:08:55 +0700

    ปรับแก้ไขในส่วนของหน้า login

--- miniproject/frontend/pages/login.js
+++ miniproject/frontend/pages/login.js
@@ -34,27 +34,32 @@ export default function Login({ token }) {
             <div>  <img class="sm:w-1/6 sm:h-1/6 items-center " src="https://cdn-icons.flaticon.com/png/512/2276/premium/2276931.png?token=exp=1650816311~hmac=6d900f76801498d89895213ff3854eb4" /> 
                 <h3 class="text-2xl font-bold text-center shadow">Login to your account</h3>
                    
-                    </div> 
-            <div>
+            </div> 
+            <br></br><br></br>
+            <div className="text-xl font-small">
                 Username:
-            </div>
+            </div><br></br>
             <div>
                 <input type="text"
                     name="username"
-                    placeholder="username"
-                    onChange={(e) => setUsername(e.target.value)}
-                />
+                    placeholder="username" className="text-xl font-medium leading-none text-gray-800"
+                    onChange={(e) => setUsername(e.target.value)} />
             </div>
-            <div>
+            <br></br><br></br>
+            <div className="text-xl font-small">
                 Password:
-            </div>
+            </div><br></br>
             <div>
                 <input type="password"
                     name="password"
-                    placeholder="password"
+                    placeholder="password" className="text-xl font-medium leading-none text-gray-800"
                     onChange={(e) => setPassword(e.target.value)} />
             </div>
-        </div>
+                <br></br><br></br><br></br>
+                <div>
+                    <button  className="bg-amber-600 mr-4 p-2 hover:text-[#EEEEEE] rounded-lg drop-shadow-lg  font-display" onClick={login}>Login</button>
+                </div>
+                </div>
     )
 
     const copyText = () => {
@@ -66,22 +71,19 @@ export default function Login({ token }) {
             <Head>
                 <title>Login</title>
             </Head>
-            <div className={styles.container}>
+
                 <Navbar />
-                <h1>Login</h1>
-                <div><b>Token:</b> {token.substring(0, 15)}...
-                <button onClick={copyText}> Copy token </button>
-                </div>
-                <br/>
+                
+                <div>
+                <div className="flex flex-col items-center justify-center">
+                    {loginForm()}
+        
                 <div>
                     Status:  {status}
                 </div>
-                <br />
-                {loginForm()}
-                <div>
-                    <button onClick={login}>Login</button>
                 </div>
-            </div>
+
+             </div>   
         </Layout>
     )
 }
